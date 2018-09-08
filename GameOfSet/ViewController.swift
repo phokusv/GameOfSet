7d0b2fea5d9302a9e27a4d1bb6a1c1b5f8085619
VJB <VJB@macbook-pro-de-vjb.local>
2018-09-08 09:58:36 -0300

    Initial Commit

---
+++ GameOfSet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfSet
+//
+//  Created by VJB on 08/09/2018.
+//  Copyright © 2018 VJB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
